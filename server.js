a169730ac5e42be03918c0f3d0cd33ed09150884
HimavanthMutte <Himu.mutte5@gmail.com>
2026-07-28 18:43:48 +0530

    Added file upload using Multer

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 import express from "express"
 import dotenv from "dotenv"
 
+import multer from "multer"
+
+const upload = multer({ dest: "uploads/" })
+
 dotenv.config()
 
 const app = express()
+
+app.post('/upload', upload.single("file"), (req, res) => {
+    try {
+        console.log(req.file)
+        return res.status(200).json({
+            message: "Uploaded Successfully!",
+            data: {
+                name: req.file.originalname,
+                format: req.file.mimetype
+            }
+        })
+    }
+    catch (err) {
+        console.log(err.message)
+        return res.status(500).json({
+            message: "Something went wrong.."
+        })
+    }
+})
 
 app.get('/health', (req, res) => {
     return res.status(200).json({
